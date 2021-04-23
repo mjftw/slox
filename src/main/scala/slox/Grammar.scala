3fb3c52e3841a59674f1bfcbf9bafa9075a18a33
Merlin Webster <mjftwebster@gmail.com>
2021-04-23 16:26:32 +0100

    More consice token type naming

--- src/main/scala/slox/Grammar.scala
+++ src/main/scala/slox/Grammar.scala
@@ -4,10 +4,10 @@ import scala.util.matching.Regex
 
 sealed trait TokenType
 
-case object IfTokenType extends TokenType
-case object IntTokenType extends TokenType
-case object VarTokenType extends TokenType
-case object EOFTokenType extends TokenType
+case object IfToken extends TokenType
+case object IntToken extends TokenType
+case object VarToken extends TokenType
+case object EOFToken extends TokenType
 
 sealed trait Literal
 
@@ -22,10 +22,10 @@ object Grammar {
   type Grammar = List[(TokenType, Regex)]
 
   implicit val grammar: Grammar = List(
-    (IfTokenType, """if"""),
-    (IntTokenType, """[0-9]+"""),
-    (VarTokenType, """[a-zA-Z]{1}[\w_]*"""),
-    (EOFTokenType, "")
+    (IfToken, """if"""),
+    (IntToken, """[0-9]+"""),
+    (VarToken, """[a-zA-Z]{1}[\w_]*"""),
+    (EOFToken, "")
   ).map({ case (token, pattern) => (token, patternToRegex(pattern)) })
 
   def patternToRegex(pattern: String): Regex = s"""(?s)^($pattern)(.*)""".r
