2cbfd3ba66ef6da788912e37369b5391f526ec42
Merlin Webster <mjftwebster@gmail.com>
2021-04-23 12:22:48 +0100

    Added basic REPL (no code eval yet)

--- src/main/scala/slox/Lox.scala
+++ src/main/scala/slox/Lox.scala
@@ -2,9 +2,12 @@ import os.Path
 import scala.util.Try
 import scala.util.Success
 import scala.util.Failure
+import scala.io._
+import scala.annotation.tailrec
 
 object Lox {
-  val usage: String = """
+  val usage: String = {
+    """
 Usage: slox [args] [script]
 
 If no script is supplied the REPL run.
@@ -12,6 +15,7 @@ If no script is supplied the REPL run.
 Args:
 -h, --help:                       Show this text
 """
+  }
 
   def main(args: Array[String]): Unit = {
     args match {
@@ -32,7 +36,32 @@ Args:
     System.exit(status)
   }
 
-  def runRepl() = ???
+  def runRepl(prompt: String = "> "): Unit = {
+    println("Starting REPL. Press Ctrl-D to exit...")
+
+    def isExit(s: String): Boolean = s match {
+      case null | "exit" => true
+      case _             => false
+    }
+
+    @tailrec
+    def loop(): Unit = {
+      print(prompt)
+
+      StdIn.readLine() match {
+        case msg if isExit(msg) => println("\nGoodbye!")
+        case code => {
+          val output = evaluate(code)
+          println(s"\n${output}")
+          loop()
+        }
+      }
+    }
+
+    loop()
+  }
 
   def runFile(file: Path) = ???
+
+  def evaluate(code: String): String = s"Mock eval of code ${code}"
 }
