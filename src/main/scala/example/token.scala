96559ff3bee970c021e78f9048236f634b93aa59
Merlin Webster <mjftwebster@gmail.com>
2021-04-23 11:01:01 +0100

    Tidy tokenisation a little

--- src/main/scala/example/token.scala
+++ src/main/scala/example/token.scala
@@ -1,6 +1,8 @@
 import scala.util.matching.Regex
 import scala.annotation.tailrec
 
+case class ParseError(message: String)
+
 sealed trait Token
 
 case class IfToken() extends Token
@@ -8,23 +10,21 @@ case class IntToken() extends Token
 case class VarToken() extends Token
 
 object Parse {
-  val lexicon: List[(String, Token)] = List(
+  val grammar = List(
     ("""if""", IfToken()),
     ("""[0-9]+""", IntToken()),
     ("""[a-zA-Z]{1}[\w_]*""", VarToken())
-  )
+  ).map(grammarToRegex)
 
-  case class ParseError(message: String)
-
-  def reLexicon(lexicon: List[(String, Token)]): List[(Regex, Token)] = for (
-    (re, token) <- lexicon
-  ) yield (s"""^($re)(.*)""".r, token)
+  def grammarToRegex(grammar: (String, Token)): (Regex, Token) = grammar match {
+    case (reStr, token) => (s"""^($reStr)(.*)""".r, token)
+  }
 
   def takeToken(
       input: String,
-      lexicon: List[(Regex, Token)]
-  ): Option[(Token, String)] = {
-    lexicon match {
+      grammar: List[(Regex, Token)]
+  ): Option[(Token, String)] =
+    grammar match {
       case Nil => None
       case (pattern, token) :: lexiconRest =>
         input match {
@@ -32,22 +32,21 @@ object Parse {
           case _                           => takeToken(input, lexiconRest)
         }
     }
-  }
 
   @tailrec
   def tokenise(
       input: String,
-      lexicon: List[(Regex, Token)],
+      grammar: List[(Regex, Token)],
       tokens: List[Token] = Nil
   ): Either[ParseError, List[Token]] =
     input match {
       case "" => Right(tokens.reverse)
       case _ =>
-        takeToken(input, lexicon) match {
+        takeToken(input, grammar) match {
           case None =>
             Left(ParseError(s"Text didn't match any pattern: $input"))
           case Some((token, restInput)) =>
-            tokenise(restInput, lexicon, token :: tokens)
+            tokenise(restInput, grammar, token :: tokens)
         }
     }
 }
