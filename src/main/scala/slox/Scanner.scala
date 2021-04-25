462be24786853fd1ff9b0aafc98739ca8b8d0d32
Merlin Webster <mjftwebster@gmail.com>
2021-04-25 08:03:35 +0100

    Scanner now handles multiline tokens correctly

--- src/main/scala/slox/Scanner.scala
+++ src/main/scala/slox/Scanner.scala
@@ -3,6 +3,7 @@ package slox
 import scala.annotation.tailrec
 import Grammar._
 import os.truncate
+import java.nio.file.Path
 
 case class SyntaxError(message: String, line: Int, column: Int)
 
@@ -12,9 +13,9 @@ object Scanner {
       grammar: Grammar,
       line: Int,
       column: Int
-  ): Option[(Token, String)] = {
+  ): Option[(Token, String, Int)] = {
     input match {
-      case ""                    => Some((Token(EOFToken, "", line, column), ""))
+      case ""                    => Some((Token(EOFToken, "", line, column), "", 0))
       case newline(_, restInput) => takeToken(restInput, grammar, line + 1, 1)
       case comment(_, restInput) => takeToken(restInput, grammar, line + 1, 1)
       case whitespace(space, restInput) =>
@@ -23,25 +24,26 @@ object Scanner {
         grammar
           .map { case (tokenType, pattern) =>
             input match {
-              case pattern(lexeme, restInput) =>
-                Some(Token(tokenType, lexeme, line, column), restInput)
+              case pattern(lexeme, restInput) => {
+                val numNewlines = "\r{0,1}\n".r.findAllIn(lexeme).length
+                Some(Token(tokenType, lexeme, line, column), restInput, line + numNewlines)
+              }
               case _ => None
             }
           }
           .maxByOption {
-            case None                              => 0
-            case Some((Token(_, lexeme, _, _), _)) => lexeme.length
+            case None                                 => 0
+            case Some((Token(_, lexeme, _, _), _, _)) => lexeme.length
           }
           .flatten
       }
     }
   }
 
-  def formatSyntaxError(error: SyntaxError): String =
+  def formatSyntaxError(error: SyntaxError, file: Option[Path] = None): String =
     s"${error.message} on line ${error.line}, ${error.column}"
 
   def scanTokens(input: String)(implicit grammar: Grammar): Either[SyntaxError, List[Token]] = {
-
     @tailrec
     def loop(
         tokens: List[Token],
@@ -52,9 +54,9 @@ object Scanner {
       takeToken(input, grammar, line, column) match {
         case None =>
           Left(SyntaxError("Syntax error", line, column))
-        case Some((token, _)) if token.tokenType == EOFToken => Right((token :: tokens).reverse)
-        case Some((token, restInput)) =>
-          loop(token :: tokens, restInput, token.line, token.column + token.lexeme.length)
+        case Some((token, _, _)) if token.tokenType == EOFToken => Right((token :: tokens).reverse)
+        case Some((token, restInput, lineNum)) =>
+          loop(token :: tokens, restInput, lineNum, token.column + token.lexeme.length)
       }
 
     loop(Nil, input, 1, 1)
