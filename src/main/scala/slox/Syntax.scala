ed7ac0990c6c668aee5f53def2562c02d529596a
Merlin Webster <mjftwebster@gmail.com>
2021-04-25 09:56:11 +0100

    Printable expressions

---
+++ src/main/scala/slox/Syntax.scala
@@ -0,0 +1,16 @@
+package slox
+
+sealed trait Expr
+case class Binary(left: Expr, operator: Token, right: Expr) extends Expr
+case class Grouping(expr: Expr) extends Expr
+case class Literal(token: Token) extends Expr
+case class Unary(operator: Token, right: Expr) extends Expr
+
+object Expr {
+  def toString(expression: Expr): String = expression match {
+    case Binary(left, op, right)         => s"(${op.lexeme} ${toString(left)} ${toString(right)})"
+    case Grouping(expr)                  => s"(group ${toString(expr)})"
+    case Literal(Token(_, lexeme, _, _)) => lexeme
+    case Unary(op, right)                => s"(${op.lexeme} ${toString(right)})"
+  }
+}
