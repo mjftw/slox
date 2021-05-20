ec7a600efcdcb829c8da95cef4704a69736bce07
Merlin Webster <mjftwebster@gmail.com>
2021-05-20 17:25:36 +0100

    Tidy Eval.evaluate

--- src/main/scala/slox/Eval.scala
+++ src/main/scala/slox/Eval.scala
@@ -5,62 +5,73 @@ import slox.lexer._
 import slox.parser._
 
 object Eval {
-  def evaluate(expression: Expr): Either[List[SyntaxError], Expr] = expression match {
-    case StringLiteral(_, _) | NumberLiteral(_, _) | BoolLiteral(_, _) | NilLiteral(_) =>
-      Right(expression)
-    case Binary(l, op, r) => {
-      val lResult = evaluate(l)
-      val rResult = evaluate(r)
+  private def evaluateBinary(expr: Binary): Either[List[SyntaxError], Expr] = {
+    val leftResult = evaluate(expr.left)
+    val rightResult = evaluate(expr.right)
 
-      (lResult, op.tokenType, rResult) match {
-        case (Left(lErrors), _, Left(rErrors)) => Left(lErrors ++ rErrors)
-        case (Left(lErrors), _, Right(_))      => Left(lErrors)
-        case (Right(_), _, Left(rErrors))      => Left(rErrors)
-        case (Right(lExpr), tokenType, Right(rExpr)) =>
-          (lExpr, tokenType, rExpr) match {
-            case (StringLiteral(lVal, _), PlusToken, StringLiteral(rVal, _)) =>
-              Right(StringLiteral(lVal ++ rVal, None))
-            case (NumberLiteral(lVal, _), PlusToken, NumberLiteral(rVal, _)) =>
-              Right(NumberLiteral(lVal + rVal, None))
-            case (NumberLiteral(lVal, _), MinusToken, NumberLiteral(rVal, _)) =>
-              Right(NumberLiteral(lVal - rVal, None))
-            case (NumberLiteral(lVal, _), StarToken, NumberLiteral(rVal, _)) =>
-              Right(NumberLiteral(lVal * rVal, None))
-            case (NumberLiteral(lVal, _), SlashToken, NumberLiteral(rVal, _)) =>
-              Right(NumberLiteral(lVal / rVal, None))
-            case (BoolLiteral(lVal, _), EqualEqualToken, BoolLiteral(rVal, _)) =>
-              Right(BoolLiteral(lVal == rVal, None))
-            case (BoolLiteral(lVal, _), BangEqualToken, BoolLiteral(rVal, _)) =>
-              Right(BoolLiteral(lVal != rVal, None))
-            case (lExpr, _, rExpr) =>
-              Left(
-                List(
-                  SyntaxError.error(
-                    s"Invalid operation: ${Expr.toString(lExpr)} ${op.lexeme} ${Expr.toString(rExpr)}"
-                  )
+    (leftResult, expr.operator.tokenType, rightResult) match {
+      case (Right(lExpr), tokenType, Right(rExpr)) =>
+        (lExpr, tokenType, rExpr) match {
+          case (StringLiteral(lVal, _), PlusToken, StringLiteral(rVal, _)) =>
+            Right(StringLiteral(lVal ++ rVal, None))
+          case (NumberLiteral(lVal, _), PlusToken, NumberLiteral(rVal, _)) =>
+            Right(NumberLiteral(lVal + rVal, None))
+          case (NumberLiteral(lVal, _), MinusToken, NumberLiteral(rVal, _)) =>
+            Right(NumberLiteral(lVal - rVal, None))
+          case (NumberLiteral(lVal, _), StarToken, NumberLiteral(rVal, _)) =>
+            Right(NumberLiteral(lVal * rVal, None))
+          case (NumberLiteral(lVal, _), SlashToken, NumberLiteral(rVal, _)) =>
+            Right(NumberLiteral(lVal / rVal, None))
+          case (BoolLiteral(lVal, _), EqualEqualToken, BoolLiteral(rVal, _)) =>
+            Right(BoolLiteral(lVal == rVal, None))
+          case (BoolLiteral(lVal, _), BangEqualToken, BoolLiteral(rVal, _)) =>
+            Right(BoolLiteral(lVal != rVal, None))
+          case (l, _, r) =>
+            Left(
+              List(
+                SyntaxError.error(
+                  s"Invalid operation: ${Expr.toString(l)} ${expr.operator.lexeme} ${Expr.toString(r)}"
                 )
               )
-          }
-      }
+            )
+        }
+      case (Left(lErrors), _, Left(rErrors)) => Left(lErrors ++ rErrors)
+      case (Left(lErrors), _, Right(_))      => Left(lErrors)
+      case (Right(_), _, Left(rErrors))      => Left(rErrors)
     }
-    case Unary(op, r) => {
-      val rResult = evaluate(r)
+  }
 
-      (op.tokenType, rResult) match {
-        case (_, Left(errors)) => Left(errors)
-        case (tokenType, Right(rExpr)) =>
-          (tokenType, rExpr) match {
-            case (MinusToken, NumberLiteral(value, _)) => Right(NumberLiteral(-value, None))
-            case (BangToken, BoolLiteral(value, _))    => Right(BoolLiteral(!value, None))
-            case _ =>
-              Left(
-                List(SyntaxError.error(s"Invalid operation: ${op.lexeme} ${Expr.toString(rExpr)}"))
+  private def evaluateUnary(expr: Unary): Either[List[SyntaxError], Expr] = {
+    val rightResult = evaluate(expr.right)
+
+    (expr.operator.tokenType, rightResult) match {
+      case (tokenType, Right(rExpr)) =>
+        (tokenType, rExpr) match {
+          case (MinusToken, NumberLiteral(value, _)) => Right(NumberLiteral(-value, None))
+          case (BangToken, BoolLiteral(value, _))    => Right(BoolLiteral(!value, None))
+          case _ =>
+            Left(
+              List(
+                SyntaxError.error(
+                  s"Invalid operation: ${expr.operator.lexeme} ${Expr.toString(rExpr)}"
+                )
               )
-          }
-      }
+            )
+        }
+      case (_, Left(errors)) => Left(errors)
     }
-    case Grouping(expr) => evaluate(expr)
-    case expression =>
-      Left(List(SyntaxError.error(s"Unsupported expression: ${Expr.toString(expression)}")))
+  }
+
+  private def evaluateGrouping(expr: Grouping): Either[List[SyntaxError], Expr] =
+    evaluate(expr.expr)
+
+  def evaluate(expr: Expr): Either[List[SyntaxError], Expr] = expr match {
+    case _: StringLiteral | _: NumberLiteral | _: BoolLiteral | _: NilLiteral =>
+      Right(expr)
+    case expr: Binary   => evaluateBinary(expr)
+    case expr: Unary    => evaluateUnary(expr)
+    case expr: Grouping => evaluateGrouping(expr)
+    case expr =>
+      Left(List(SyntaxError.error(s"Unsupported expression: ${Expr.toString(expr)}")))
   }
 }
